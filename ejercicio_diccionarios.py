4635e1efa947f9babecfd61fe134cf335d320658
Ghariel <arielgarzon495@gmail.com>
2021-06-14 18:56:17 -0300

    agregué un método de búsqueda para no usar for

--- ejercicio_diccionarios.py
+++ ejercicio_diccionarios.py
@@ -9,23 +9,40 @@
 # Tiene que ser una funcion
 
 # Lista de diccionarios
-alumnos = [
-    {"nombre": "Pepe", "apellido": "Perez", "matricula": 435},
-    {"nombre": "Julieta", "apellido": "Rodriguez", "matricula": 233},
-    {"nombre": "Maria", "apellido": "Rodriguez", "matricula": 123}
-]
+#alumnos = [
+#    {"nombre": "Pepe", "apellido": "Perez", "matricula": 435},
+#    {"nombre": "Julieta", "apellido": "Rodriguez", "matricula": 233},
+#    {"nombre": "Maria", "apellido": "Rodriguez", "matricula": 123}
+#]
 
-def listar_alumnos(lista_alumnos):
-    for a in lista_alumnos:
-        print(a["nombre"])
+#mi diccionario modificado
+alumnos = {
+    435:{"nombre": "Pepe", "apellido": "Perez"}, 
+    233:{"nombre": "Julieta", "apellido": "Rodriguez"},
+    123:{"nombre": "Maria", "apellido": "Rodriguez"}
+}
 
-def obtener_alumno(matricula, lista_alumnos):
-    for a in lista_alumnos:
-        if a['matricula'] == matricula:
-            return a
+#def listar_alumnos(lista_alumnos):
+#    for a in lista_alumnos:
+#        print(a["nombre"])
 
+#def obtener_alumno(matricula, lista_alumnos):
+#    for a in lista_alumnos:
+#        if a['matricula'] == matricula:
+#            return a
 
-listar_alumnos(alumnos)
-print(alumnos)
-mi_alumno = obtener_alumno(435, alumnos)
-print(mi_alumno)
+def buscar_matricula():
+   #para evitar error al ingresar un dato que no sea entero
+   while True:
+       try: 
+           matricula = int(input("Introduzca el numero de matricula: "))
+            break
+       except ValueError:
+            print("El dato ingresado no es valido :c")
+    print(alumnos.get(matricula, "No se encontro ningun alunmo que coincida con la matricula ingresada"))
+
+buscar_matricula()    
+#listar_alumnos(alumnos)
+#print(alumnos)
+#mi_alumno = obtener_alumno(435, alumnos)
+#print(mi_alumno)
